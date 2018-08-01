0679f29081cc79c621e1ca725b9a3f487b6ab417
coderlifter <ercalbwar@gmail.com>
2018-08-01 17:47:10 +0100

    Blurry Background

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import '../styles/Introduction.css';
-import Background from '../resources/background.jpg'
 
 class Introduction extends Component {
   render() {
     return (
-      <div id='intro' className='IntroductionContainer' style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover' }}>
-        <hr className='IntroductionHr' />
-        <h1>JORGE RODRÍGUEZ CALDADO</h1>
-        <h2>Estudiante de Grado de Arquitectura</h2>
-        <h2>Técnico Superior en Proyectos de Edificación</h2>
-        <hr className='IntroductionHr' />
+      <div className='IntroductionContainer'>
+        <div className='IntroductionImage' />
+        <div id='intro' className='Introduction' >
+          <hr className='IntroductionHr' />
+          <h1>JORGE RODRÍGUEZ CALDADO</h1>
+          <h2>Estudiante de Grado de Arquitectura</h2>
+          <h2>Técnico Superior en Proyectos de Edificación</h2>
+          <hr className='IntroductionHr' />
+        </div>
       </div>
     );
   }
