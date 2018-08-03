6ef7cbf350081cf3ff3481144fa8b0fb862d7247
coderlifter <ercalbwar@gmail.com>
2018-08-03 17:27:43 +0100

    About Me Mobile version

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,14 +8,16 @@ class AboutMe extends Component {
     return (
       <div id='about' className='AboutMeContainer'>
         <SectionHeader name='Sobre mí' />
-        <div className='AboutMe Text'>
-          ¡Hola!
+        <div className='AboutMe'>
+          <div className='Text'>
+            ¡Hola!
             <br /><br />
-          Soy Jorge y, desde pequeño, estoy muy interesado la Arquitectura y cómo se diseñaban los edificios.
-          Cuando entré en la Escuela Técnica Superior de Arquitectura de Málaga, empecé a tener contacto con el Urbanismo,
-          y hoy día me parece una disciplina fundamental en el desarrollo de las ciudades.
-          Creo firmemente que, tanto la Arquitectura como el Urbanismo, deben estar basadas en la eficiencia energética, el diseño bioclimático para el desarrollo de las personas
-          y el cuidado de la naturaleza.
+            Soy Jorge y, desde pequeño, estoy muy interesado la Arquitectura y cómo se diseñaban los edificios.
+            Cuando entré en la Escuela Técnica Superior de Arquitectura de Málaga, empecé a tener contacto con el Urbanismo,
+            y hoy día me parece una disciplina fundamental en el desarrollo de las ciudades.
+            Creo firmemente que, tanto la Arquitectura como el Urbanismo, deben estar basadas en la eficiencia energética, el diseño bioclimático para el desarrollo de las personas
+            y el cuidado de la naturaleza.
+          </div>
           <img src={ProfileImage} className='ProfileImage' alt='Me' />
         </div>
       </div>
