4b7aa551f846e390f8b3b1d5eb19a2e195bfb809
coderlifter <ercalbwar@gmail.com>
2018-08-05 11:51:20 +0100

    Portfolio Image shows title on hover

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -17,31 +17,30 @@ class Project extends Component {
     const { photoIndex, isOpen } = this.state;
 
     return (
-      <div>
-        <div className='ProjectContainer'>
-          <div className='ProjectImageContainer' onClick={() => this.setState({ isOpen: true })}>
-            <img src={this.props.images[0]} className='ProjectImage' alt={`${this.props.title} 1`} />
-          </div>
+      <div className='ProjectContainer'>
+        <div className='ProjectImageContainer' onClick={() => this.setState({ isOpen: true })}>
+          <img src={this.props.images[0]} className='ProjectImage' alt={`${this.props.title} 1`} />
+          <p className='ProjectImageTitle'>{this.props.title}</p>
         </div>
 
         {isOpen && (
-            <Lightbox
-              mainSrc={this.props.images[photoIndex]}
-              nextSrc={this.props.images[(photoIndex + 1) % this.props.images.length]}
-              prevSrc={this.props.images[(photoIndex + this.props.images.length - 1) % this.props.images.length]}
-              onCloseRequest={() => this.setState({ isOpen: false })}
-              onMovePrevRequest={() =>
-                this.setState({
-                  photoIndex: (photoIndex + this.props.images.length - 1) % this.props.images.length,
-                })
-              }
-              onMoveNextRequest={() =>
-                this.setState({
-                  photoIndex: (photoIndex + 1) % this.props.images.length,
-                })
-              }
-            />
-          )}
+          <Lightbox
+            mainSrc={this.props.images[photoIndex]}
+            nextSrc={this.props.images[(photoIndex + 1) % this.props.images.length]}
+            prevSrc={this.props.images[(photoIndex + this.props.images.length - 1) % this.props.images.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + this.props.images.length - 1) % this.props.images.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % this.props.images.length,
+              })
+            }
+          />
+        )}
       </div>
     );
   }
